22e9d1ef702fbe23a8a0ddd0293570587efddb25
akib413 <hasanakibul216@gmail.com>
2026-04-18 11:48:31 +0600

    add css style in all countries

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
-const Country = ({country}) => {
-    console.log(country.population.population)
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
+    // console.log(country.area.area)
+
+    const handleVisited = () => {
+        // setVisited(visited ? false : true)
+
+        setVisited(!visited)
+
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+    }
+
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.png.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? 'Big Country' : 'Small Country'}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };
